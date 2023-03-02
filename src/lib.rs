e331ddcfbf59578e231141f0924a0e58e3be11eb
nabetama <mao.nabeta@gmail.com>
2023-03-02 14:58:20 +0900

    calculate amout with tax

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,51 @@
-use std::time::SystemTime;
+use chrono::{Datelike, NaiveDate};
 
-// const RATE100: f32 = 1.0 / 1.0;
-// const RATE103: f32 = 1.03;
-// const RATE105: f32 = 1.05;
-// const RATE108: f32 = 1.08;
-const RATE110: f32 = 1.1_f32;
+const RATE100: f64 = 1.0_f64;
+const RATE103: f64 = 1.03_f64;
+const RATE105: f64 = 1.05_f64;
+const RATE108: f64 = 1.08_f64;
+const RATE110: f64 = 1.1_f64;
 
-pub fn amount_with_tax(amount: i64) -> i64 {
+pub fn amount_with_tax<T>(amount: i64, today: Option<T>) -> i64
+where
+    T: Datelike,
+{
     if amount < 0 {
         return amount;
     }
 
-    let res = RATE110 * amount as f32;
-    res as i64
+    match today {
+        Some(date) => {
+            let res = get_rate::<T>(date) * amount as f64;
+            return res as i64;
+        }
+        None => return amount,
+    }
+}
+
+fn get_rate<T>(date: T) -> f64
+where
+    T: Datelike,
+{
+    let days = date.num_days_from_ce();
+
+    if nd_from_ce(1989, 4, 1) <= days && days <= nd_from_ce(1997, 3, 31) {
+        return RATE103;
+    }
+    if nd_from_ce(1997, 4, 1) <= days && days <= nd_from_ce(2014, 3, 31) {
+        return RATE105;
+    }
+    if nd_from_ce(2014, 4, 1) <= days && days <= nd_from_ce(2019, 9, 30) {
+        return RATE108;
+    }
+    if nd_from_ce(2019, 10, 1) <= days {
+        return RATE110;
+    }
+
+    RATE100
+}
+
+fn nd_from_ce(y: i32, m: u32, d: u32) -> i32 {
+    let days = NaiveDate::from_ymd_opt(y, m, d).unwrap();
+    days.num_days_from_ce()
 }
--- tests/vat.rs
+++ tests/vat.rs
@@ -1,8 +1,107 @@
-use std::time::SystemTime;
-
+use chrono::NaiveDate;
 use vat_jp;
 
 #[test]
-fn test_vat_jp() {
-    assert_eq!(110, vat_jp::amount_with_tax(100));
+fn test_amout_with_tax() {
+    let mut today = NaiveDate::from_ymd_opt(1989, 3, 31).unwrap();
+
+    assert_eq!(100, vat_jp::amount_with_tax::<NaiveDate>(100, None));
+
+    assert_eq!(100, vat_jp::amount_with_tax::<NaiveDate>(100, Some(today)));
+    assert_eq!(
+        -100,
+        vat_jp::amount_with_tax::<NaiveDate>(-100, Some(today))
+    );
+    assert_eq!(
+        4200,
+        vat_jp::amount_with_tax::<NaiveDate>(4200, Some(today))
+    );
+    assert_eq!(
+        -100,
+        vat_jp::amount_with_tax::<NaiveDate>(-100, Some(today))
+    );
+
+    today = NaiveDate::from_ymd_opt(1989, 4, 1).unwrap();
+
+    assert_eq!(103, vat_jp::amount_with_tax::<NaiveDate>(100, Some(today)));
+    assert_eq!(
+        -100,
+        vat_jp::amount_with_tax::<NaiveDate>(-100, Some(today))
+    );
+    assert_eq!(
+        4326,
+        vat_jp::amount_with_tax::<NaiveDate>(4200, Some(today))
+    );
+
+    today = NaiveDate::from_ymd_opt(1997, 3, 31).unwrap();
+
+    assert_eq!(103, vat_jp::amount_with_tax::<NaiveDate>(100, Some(today)));
+    assert_eq!(
+        -100,
+        vat_jp::amount_with_tax::<NaiveDate>(-100, Some(today))
+    );
+    assert_eq!(
+        4326,
+        vat_jp::amount_with_tax::<NaiveDate>(4200, Some(today))
+    );
+
+    today = NaiveDate::from_ymd_opt(1997, 4, 1).unwrap();
+
+    assert_eq!(105, vat_jp::amount_with_tax::<NaiveDate>(100, Some(today)));
+    assert_eq!(
+        -100,
+        vat_jp::amount_with_tax::<NaiveDate>(-100, Some(today))
+    );
+    assert_eq!(
+        4410,
+        vat_jp::amount_with_tax::<NaiveDate>(4200, Some(today))
+    );
+
+    today = NaiveDate::from_ymd_opt(2014, 3, 31).unwrap();
+
+    assert_eq!(105, vat_jp::amount_with_tax::<NaiveDate>(100, Some(today)));
+    assert_eq!(
+        -100,
+        vat_jp::amount_with_tax::<NaiveDate>(-100, Some(today))
+    );
+    assert_eq!(
+        4410,
+        vat_jp::amount_with_tax::<NaiveDate>(4200, Some(today))
+    );
+
+    today = NaiveDate::from_ymd_opt(2014, 4, 1).unwrap();
+
+    assert_eq!(108, vat_jp::amount_with_tax::<NaiveDate>(100, Some(today)));
+    assert_eq!(
+        -100,
+        vat_jp::amount_with_tax::<NaiveDate>(-100, Some(today))
+    );
+    assert_eq!(
+        4536,
+        vat_jp::amount_with_tax::<NaiveDate>(4200, Some(today))
+    );
+
+    today = NaiveDate::from_ymd_opt(2019, 9, 30).unwrap();
+
+    assert_eq!(108, vat_jp::amount_with_tax::<NaiveDate>(100, Some(today)));
+    assert_eq!(
+        -100,
+        vat_jp::amount_with_tax::<NaiveDate>(-100, Some(today))
+    );
+    assert_eq!(
+        4536,
+        vat_jp::amount_with_tax::<NaiveDate>(4200, Some(today))
+    );
+
+    today = NaiveDate::from_ymd_opt(2019, 10, 1).unwrap();
+
+    assert_eq!(110, vat_jp::amount_with_tax::<NaiveDate>(100, Some(today)));
+    assert_eq!(
+        -100,
+        vat_jp::amount_with_tax::<NaiveDate>(-100, Some(today))
+    );
+    assert_eq!(
+        4620,
+        vat_jp::amount_with_tax::<NaiveDate>(4200, Some(today))
+    );
 }
